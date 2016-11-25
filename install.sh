b727c49fc3c7879dfd867190ce6ec3ed62fb01ee
root <root@archiso.numericable.fr>
2016-11-25 09:21:26 +0000

    Update script install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
-#!/bin/sh
+#!/bin/zsh
 
-rsync ./rootfs/ / --progress
+rsync --progress -r ./rootfs/ /
+loadkeys mac-fr-ext_new
+
+timedatectl set-ntp true
+
+
+
+echo -e "n\np\n1\n2048\n\nw\n" |fdisk /dev/sda
+
+mkfs.ext4 /dev/sda1
+
+
+mount /dev/sda1 /mnt
+
+
+
+
+pacstrap /mnt base
